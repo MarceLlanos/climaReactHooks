6e14d33179fd297366fa101d35d87cfb0898d58e
Marcela Llanos <marcela3llanos@gmail.com>
2019-10-17 17:52:52 -0400

    Se agrego el componente de mostrar Clima y los error segun a lo que pida

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Error from './components/Error';
+import Clima from './components/Clima';
 
 
 function App() {
 
   const [ciudad, guardarCiudad] = useState('');
   const [pais, guardarPais] = useState('');
-  const [error, guardarError] = useState(false)
+  const [error, guardarError] = useState(false);
+  const [resultado, guardaResultado] = useState({});
+
+  useEffect(()=>{
+    //prevenir ejecucion primera vez
+    if(ciudad === '') return;
+
+    const consultarAPI = async ()=>{
+      const appID='bf35dd2f96229c44a5203ef2e283288e';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
+  
+      //consultar la url
+  
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      guardaResultado(resultado);
+    }
+
+    consultarAPI();
+  },[ciudad, pais]);
 
   const datosConsulta = datos => {
     // Validad que ambos campos esten
@@ -22,7 +42,7 @@ function App() {
     guardarPais(datos.pais);
     guardarError(false);
 
-  } 
+  }
 
   //Cargar un componente condicionalmente
   let componente;
@@ -31,9 +51,13 @@ function App() {
     componente = <Error
       mensaje = 'Ambos campos con obligatorios'
     />
+  }else if(resultado.cod==='404'){
+      componente =<Error mensaje='La ciudad no existe en el pais'/>
   }else{
     //Mostrar el clima
-    componente = null
+    componente = <Clima 
+                  resultado={resultado}
+                />
   }
 
   return ( 
